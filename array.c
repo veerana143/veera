26dd83f8d02488d32ef7f3aea176ddc6a51cff27
veera <narayanac76@gmail.com>
2024-01-20 10:34:21 +0530

    changed

---
+++ array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int i,f, n;
+    int arr[10]={1,2,3,4,5,6,7,8};
+    printf("searching element\n");
+    scanf("%d",&f);
+     for(i=0; i<n; i++)
+    {
+        if(arr[i]==f)
+        {
+	    printf("find element %d\n", f);
+        }
+ 
+    }
+
+}
+
+
+
